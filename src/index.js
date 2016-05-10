f4081c8072c8c5908d19d2ded7e9095657c49c0d
Jarkko Mönkkönen <jamonkko@gmail.com>
2016-05-10 23:36:15 +0300

    use babel-plugin-rename-assigned-properties

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
-export default ({ types: t }) => {
-  const buildPropertyAssignment = (objName, propName, valueNode) =>
-    t.assignmentExpression(
-      '=',
-      t.memberExpression(t.identifier(objName), t.identifier(propName)),
-      valueNode
-    )
+import renameProperties from 'babel-plugin-rename-assigned-properties'
 
-  return {
-    visitor: {
-      Program: {
-        exit(program, { opts }) {
-          program.traverse({
-            AssignmentExpression: {
-              enter(path) {
-                if (path.node.operator !== '=' || !t.isMemberExpression(path.node.left)) {
-                  return
-                }
-                for (const name of Object.keys(opts)) {
-                  if (!t.isIdentifier(path.node.left.object, { name: 'global' })
-                    || !t.isIdentifier(path.node.left.property, { name })) {
-                    continue
-                  }
-                  const [to, ...aliases] = [].concat(opts[name])
-                  const renamedAssignment = buildPropertyAssignment('global', to, path.node.right)
-                  const chainedAliasAssignments = aliases.reduce(
-                    (chained, alias) =>
-                      buildPropertyAssignment('global', alias, chained),
-                    renamedAssignment
-                  )
-                  path.replaceWith(t.expressionStatement(chainedAliasAssignments))
-                }
-              }
+export default (babel) => ({
+  visitor: {
+    Program: {
+      exit(program, { opts }) {
+        renameProperties(babel).visitor.Program.exit(
+          program,
+          {
+            opts: {
+              renames: {
+                global: opts
+              },
+              process: 'post'
             }
-          })
-        }
+          }
+        )
       }
     }
   }
-}
+})
