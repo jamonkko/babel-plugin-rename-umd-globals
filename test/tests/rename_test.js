a83936a08fcb38fa4e0053581565c6e7b63867a2
Jarkko Mönkkönen <jarkko@Jarkko-MacBook-Air.local>
2016-05-14 17:04:16 +0300

    added tests

---
+++ test/tests/rename_test.js
@@ -0,0 +1,45 @@
+import { transform } from 'babel-core'
+
+suite('renaming globals', () => {
+  test('rename single global', () => {
+    const result = transform(
+      'global.name = 0;',
+      {
+        plugins: [
+          ['./src/index.js', { name: 'NewName' }]
+        ]
+      }
+    )
+    result.code.should.equal('global.NewName = 0;')
+  })
+
+  test('rename with aliases', () => {
+    const result = transform(
+      'global.name = 0;',
+      {
+        plugins: [
+          ['./src/index.js', { name: ['NewName', 'NN', 'NewN'] }]
+        ]
+      }
+    )
+    result.code.should.equal('global.NewN = global.NN = global.NewName = 0;')
+  })
+
+  test('rename with multiple properties', () => {
+    const result = transform(
+      'global.name1 = 0;global.name2 = 1;global.noRename = 2;',
+      {
+        plugins: [
+          ['./src/index.js', {
+            name1: 'NewName1',
+            name2: ['NewName2', 'NN'],
+            unknown: 'NoEffect'
+          }]
+        ]
+      }
+    )
+    result.code.should.equal(
+      'global.NewName1 = 0;global.NN = global.NewName2 = 1;global.noRename = 2;')
+  })
+})
+
