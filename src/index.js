b69487b080e1e2cee2d4b5284e77856acad6a67f
Jarkko Mönkkönen <jamonkko@gmail.com>
2017-06-07 23:49:02 +0300

    comment header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*!
+ * @license
+ * babel-plugin-rename-umd-globals v1.0.0 (https://github.com/jamonkko/babel-plugin-rename-umd-globals#readme)
+ * Copyright 2017 Jarkko Mönkkönen <jamonkko@gmail.com>
+ * Licensed under MIT
+ */
 import renameProperties from 'babel-plugin-rename-assigned-properties'
 
 export default babel => ({
